56654796b76c8764ce6588ab52c877d092eb1664
kokiiii0207 <kokiiii12189527@gmail.com>
2016-02-11 21:41:37 +0900

    Initial Commit

---
+++ clap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clap
+//
+//  Created by 木村　恒輝 on 2016/02/11.
+//  Copyright (c) 2016年 木村　恒輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
